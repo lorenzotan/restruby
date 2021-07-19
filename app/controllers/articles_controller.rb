1d2339b63b8fec6af79aafb90d6080ed9513006a
Lorenzo Tan <lotan@firstam.com>
2021-07-19 16:53:49 -0700

    completed basic CRUD app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,13 +13,31 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    #@article = Article.new(title: params[:article][:title], body: params[:article][:title])
 
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
   end
 
   private
